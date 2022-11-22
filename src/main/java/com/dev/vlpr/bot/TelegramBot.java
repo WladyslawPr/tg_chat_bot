86a520f96790cacf4731d67d777495b265bceb59
vladyslavprydacha@MacBook <user.waw3@gmail.com>
2022-11-22 17:43:30 +0100

    added log4j and update pom.file.

--- src/main/java/com/dev/vlpr/bot/TelegramBot.java
+++ src/main/java/com/dev/vlpr/bot/TelegramBot.java
@@ -1,10 +1,14 @@
 package com.dev.vlpr.bot;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Log4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
     @Value("${bot.name}")
@@ -25,6 +29,21 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         var originalMessage = update.getMessage();
-        System.out.println(originalMessage.getText());
+        log.debug(originalMessage.getText());
+
+        var response = new SendMessage();
+        response.setChatId(originalMessage.getChatId().toString());
+        response.setText("reverse response");
+        sendAnswerMessage(response);
+    }
+
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException exception) {
+                log.error(exception);
+            }
+        }
     }
 }
