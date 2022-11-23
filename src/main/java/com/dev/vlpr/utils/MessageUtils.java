a654b44ddfbfc117fe50608c5b66d62653494277
vlpr <user.waw3@gmail.com>
2022-11-23 16:00:20 +0100

    added message processing and service.

--- src/main/java/com/dev/vlpr/utils/MessageUtils.java
+++ src/main/java/com/dev/vlpr/utils/MessageUtils.java
@@ -1,8 +1,10 @@
 package com.dev.vlpr.utils;
 
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+@Component
 public class MessageUtils {
 
     public SendMessage generateSendMessageWithText(Update update, String text) {
