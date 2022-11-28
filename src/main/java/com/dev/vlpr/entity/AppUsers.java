e2a69de10e31d8044c6c1fe94722ecd639ca9506
vlpr <user.waw3@gmail.com>
2022-11-28 18:33:17 +0100

    added user entity and enum which will store the state.

---
+++ src/main/java/com/dev/vlpr/entity/AppUsers.java
@@ -0,0 +1,31 @@
+package com.dev.vlpr.entity;
+
+import com.dev.vlpr.entity.enums.UserState;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "app_users")
+public class AppUsers {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long telegramUserId;
+    @CreationTimestamp
+    private LocalDateTime firstLoginDate;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private Boolean isActive;
+    @Enumerated(EnumType.STRING)
+    private UserState state;
+}
