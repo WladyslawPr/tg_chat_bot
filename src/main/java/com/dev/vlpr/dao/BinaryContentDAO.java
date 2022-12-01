acffbd34b3f3afb52eb7f1cbeb7fc09b60f69b4e
vlpr <user.waw3@gmail.com>
2022-12-01 15:13:57 +0100

    added entity.

---
+++ src/main/java/com/dev/vlpr/dao/BinaryContentDAO.java
@@ -0,0 +1,7 @@
+package com.dev.vlpr.dao;
+
+import com.dev.vlpr.entity.BinaryContent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BinaryContentDAO extends JpaRepository<BinaryContent, Long> {
+}
---
+++ src/main/java/com/dev/vlpr/entity/AppDocument.java
@@ -0,0 +1,24 @@
+package com.dev.vlpr.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "app_document")
+public class AppDocument {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String telegramField;
+    private String docName;
+    @OneToOne
+    private BinaryContent binaryContent;
+    private String mimeType;
+    private Long fileSize;
+}
