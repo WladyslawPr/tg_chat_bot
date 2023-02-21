2da65000ab9ca4ad0796b874aa7e5d02d26d132f
vlpr <user.waw3@gmail.com>
2023-02-21 16:48:19 +0100

    added file id encryption

---
+++ src/main/java/com/dev/vlpr/entity/enums/LinkType.java
@@ -0,0 +1,17 @@
+package com.dev.vlpr.entity.enums;
+
+public enum LinkType {
+
+    GET_DOC("file/get-doc"),
+    GET_PHOTO("file/get-photo");
+
+    private final String link;
+
+    LinkType(String link) {
+        this.link = link;
+    }
+
+    public String toString() {
+        return link;
+    }
+}
--- src/main/java/com/dev/vlpr/service/FileService.java
+++ src/main/java/com/dev/vlpr/service/FileService.java
@@ -2,9 +2,12 @@ package com.dev.vlpr.service;
 
 import com.dev.vlpr.entity.AppDocument;
 import com.dev.vlpr.entity.AppPhoto;
+import com.dev.vlpr.entity.enums.LinkType;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
 public interface FileService {
     AppDocument processDoc(Message telegramMessage);
     AppPhoto processPhoto(Message telegramMessage);
+
+    String generateLink(Long docId, LinkType linkType);
 }
